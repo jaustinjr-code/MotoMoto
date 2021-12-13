dded81a68bf46966ad5d0d64b3c820879a81063a
Jacob Nagano-Sunia <jacob.sunia@student.csulb.edu>
2021-12-13 14:23:31 -0800

    Updated USER MANAGEMENT SQL Code

--- SetupEnvironment/MariaDB/MotoMotoUserManagement.sql
+++ SetupEnvironment/MariaDB/MotoMotoUserManagement.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Type (
     typeId INT NOT NULL AUTO_INCREMENT,
     typeName VARCHAR(25) NOT NULL,
-    CONSTRAINT Type_PK PRIMARY KEY (typeId)
+    CONSTRAINT Type_PK PRIMARY KEY (typeId),
+    CONSTRAINT Type_UK UNIQUE (typeName)
 );
 
 CREATE TABLE User (
@@ -10,8 +11,10 @@ CREATE TABLE User (
     username VARCHAR(25) NOT NULL,
     password VARCHAR(25) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    able TINYINT(1) NOT NULL,
-    eventAccount TINYINT(1) NOT NULL,
+    able BOOL NOT NULL,
+    eventAccount BOOL NOT NULL,
     CONSTRAINT User_PK PRIMARY KEY (userId),
+    CONSTRAINT Username_UK UNIQUE (username),
+    CONSTRAINT Email_UK UNIQUE (email),
     CONSTRAINT User_Type_FK FOREIGN KEY (typeId) REFERENCES Type (typeId)    
 );
