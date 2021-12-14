cadd085aa989f066afa6a4aa626b0e5a23d5339f
bobdidley <james.austin@student.csulb.edu>
2021-12-14 11:12:57 -0800

    updated sql error for log db

--- SetupEnvironment/MariaDB/logs_backup.sql
+++ SetupEnvironment/MariaDB/logs_backup.sql
@@ -23,10 +23,10 @@ DROP TABLE IF EXISTS `Category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Category` (
-  `categoryName` varchar(100) NOT NULL,
+  `categoryName` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`categoryName`),
   UNIQUE KEY `categoryName` (`categoryName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,10 +47,10 @@ DROP TABLE IF EXISTS `Level`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Level` (
-  `levelName` varchar(50) NOT NULL,
+  `levelName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`levelName`),
   UNIQUE KEY `levelName` (`levelName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,17 +72,17 @@ DROP TABLE IF EXISTS `Log`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Log` (
   `logId` int(11) NOT NULL AUTO_INCREMENT,
-  `categoryName` varchar(100) NOT NULL,
-  `levelName` varchar(100) NOT NULL,
-  `timeStamp` time NOT NULL DEFAULT current_timestamp(),
+  `categoryName` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `levelName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `timeStamp` datetime NOT NULL DEFAULT current_timestamp(),
   `userID` int(11) NOT NULL,
-  `DSCRIPTION` varchar(1000) NOT NULL,
+  `DSCRIPTION` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`logId`),
   KEY `Log_Category_FK` (`categoryName`),
   KEY `Log_Level_FK` (`levelName`),
   CONSTRAINT `Log_Category_FK` FOREIGN KEY (`categoryName`) REFERENCES `Category` (`categoryName`),
   CONSTRAINT `Log_Level_FK` FOREIGN KEY (`levelName`) REFERENCES `Level` (`levelName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-13 15:32:15
+-- Dump completed on 2021-12-14 11:11:49
