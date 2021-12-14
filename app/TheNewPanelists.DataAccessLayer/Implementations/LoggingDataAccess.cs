15f2836a69ee4a313a1387c2ca11b0f64f122b92
bobdidley <james.austin@student.csulb.edu>
2021-12-14 13:10:16 -0800

    BuildTempUser fixed, still haven't generalized db name

--- app/TheNewPanelists.DataAccessLayer/Implementations/LoggingDataAccess.cs
+++ app/TheNewPanelists.DataAccessLayer/Implementations/LoggingDataAccess.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using MySql.Data.MySqlClient;
 // using System.Data.Odbc;
 using System.Collections;
+using System.Text;
 
 
 namespace TheNewPanelists.DataAccessLayer
@@ -11,7 +12,7 @@ namespace TheNewPanelists.DataAccessLayer
         private string operation { get; set; }
         private bool isSuccess { get; }
         private Dictionary<string, string> log { get; set; }
-        //private MySqlConnection mySqlConnection { get; set; }
+        private MySqlConnection mySqlConnection = null;
 
         public LoggingDataAccess(string operation, bool isSuccess)//, Dictionary<string, string> log)
         {
@@ -67,32 +68,61 @@ namespace TheNewPanelists.DataAccessLayer
             return false;
         }
 
-        // private void BuildTempUser(MySqlConnection mySqlConnection)
-        // {
-        //     using (mySqlConnection = new MySqlConnection("server=localhost;user=root;"))
-        //     using (MySqlCommand cmd = new MySqlCommand("CREATE USER 'tempuser'@'localhost' IDENTIFIED BY '123';", mySqlConnection))
-        //     {
-        //         try
-        //         {
-        //             mySqlConnection.Open();
-        //             Console.WriteLine("Connection Open...");
-        //             cmd.ExecuteNonQuery();
-        //             Console.WriteLine("Temp User Created...");
-        //         }
-        //         catch (Exception e)
-        //         {
-        //             Console.WriteLine("Exited Program with Exit " + e);
-        //         }
-        //         mySqlConnection.Close();
-        //     }
-        //     EstablishMariaDBConnection();
-        // }
+        private void BuildTempUser(MySqlConnection mySqlConnection)
+        {
+            // Hides password
+            StringBuilder input = new StringBuilder();
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter) break;
+                if (key.Key == ConsoleKey.Backspace && input.Length > 0) input.Remove(input.Length - 1, 1);
+                else if (key.Key != ConsoleKey.Backspace) input.Append(key.KeyChar);
+            }
+            string pass = input.ToString();
+            string user = System.Environment.UserName;
+
+
+            //Console.WriteLine(pass);
+            //Console.WriteLine(System.Environment.UserName);
+
+            MySqlConnection tempMySqlConnection = new MySqlConnection($"server=localhost;user={user};password={pass}");
+            try
+            {
+                tempMySqlConnection.Open();
+                // MySqlCommand cmd1 = new MySqlCommand("DROP USER IF EXISTS 'tempuser'@'localhost';", tempMySqlConnection);
+                MySqlCommand cmd2 = new MySqlCommand("CREATE USER IF NOT EXISTS 'tempuser'@'localhost' IDENTIFIED BY '123';", tempMySqlConnection);
+                MySqlCommand cmd3 = new MySqlCommand("GRANT ALL PRIVILEGES ON *.* TO 'tempuser'@'localhost' WITH GRANT OPTION;", tempMySqlConnection);
+                MySqlCommand cmd4 = new MySqlCommand("FLUSH PRIVILEGES;", tempMySqlConnection);
+                // MySqlCommand cmd4 = new MySqlCommand("SHOW DATABASE LIKE logs_MM_test;", tempMySqlConnection);
+                // MySqlCommand cmd5 = new MySqlCommand("CREATE DATABASE IF NOT EXISTS logs_MM_test;", tempMySqlConnection);
+
+                Console.WriteLine("Connection Open...");
+                // cmd1.ExecuteNonQuery();
+                Console.WriteLine("DROP");
+                cmd2.ExecuteNonQuery();
+                Console.WriteLine("GRANT");
+                cmd3.ExecuteNonQuery();
+                Console.WriteLine("FLUSH");
+                cmd4.ExecuteNonQuery();
+                Console.WriteLine("CREATE");
+                // cmd5.ExecuteNonQuery();
+                Console.WriteLine("Temp User Created...");
+                tempMySqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exited Program with Exit " + e.Message);
+            }
+            EstablishMariaDBConnection();
+        }
 
         public bool EstablishMariaDBConnection()
         {
-            MySqlConnection mySqlConnection;
+            // MySqlConnection mySqlConnection;
             // This is a hardcoded string, it will be different based on your naming
-            string connectionString = "server=localhost;user=admin_MM_test;database=logs_MM_test;port=3306;password=123;";
+            // Need to generalize the database name or create a new database and run the restore sql file on it
+            string connectionString = "server=localhost;user=tempuser;database=logs_MM_test;port=3306;password=123;";
             // string connectionString = "server=localhost;user=tempuser;database=logs_MM_test;port=3306;";
 
             try
@@ -103,19 +133,19 @@ namespace TheNewPanelists.DataAccessLayer
                 // SqlGenerator
                 MySqlCommand command = new MySqlCommand(SqlGenerator(), mySqlConnection);
                 // Console.WriteLine(command.ExecuteNonQuery());
-                if (command.ExecuteNonQuery() == 1)
-                {
-                    Console.WriteLine("Close");
-                    mySqlConnection.Close();
+                // if (command.ExecuteNonQuery() == 1)
+                // {
+                Console.WriteLine("Close");
+                mySqlConnection.Close();
 
-                    return true;
-                }
+                return true;
+                // }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                // Console.WriteLine("ERROR - Creating new user...");
-                // BuildTempUser(mySqlConnection);
+                Console.WriteLine("ERROR - Creating new user...");
+                BuildTempUser(mySqlConnection);
             }
 
             return false;
