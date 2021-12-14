3209d819af838b4af14c9b974396dd4ace41f4ef
Jacob Nagano-Sunia <jacob.sunia@student.csulb.edu>
2021-12-13 20:01:39 -0800

    Added insert statements and SqlGenerator

--- app/TheNewPanelists.DataAccessLayer/Implementations/UserManagementDataAccess.cs
+++ app/TheNewPanelists.DataAccessLayer/Implementations/UserManagementDataAccess.cs
@@ -10,21 +10,26 @@ namespace TheNewPanelists.DataAccessLayer
         private string operation { get; set; }
 
         private MySqlConnection mySqlConnection = null;
-        public UserManagementDataAccess (string operation, Dictionary<string, string> userInfo) {
+        public UserManagementDataAccess (string operation, Dictionary<string, string> userInfo) 
+        {
             this.userInfo = userInfo;
             this.operation = operation;
         }
 
-        private void BuildTempUser() {
+        private void BuildTempUser() 
+        {
             using(mySqlConnection = new MySqlConnection("server=localhost;user=MotoMotoA;password=password;"))
             using (MySqlCommand cmd = new MySqlCommand("CREATE USER 'tempuser'@'localhost' IDENTIFIED BY 'password';", mySqlConnection))
             {
-                try {
+                try 
+                {
                     mySqlConnection.Open();
                     Console.WriteLine("Connection Open...");
                     cmd.ExecuteNonQuery();
                     Console.WriteLine("Temp User Created...");
-                } catch (Exception e) {
+                } 
+                catch (Exception e) 
+                {
                     Console.WriteLine("Exited Program with Exit "+e);
                 }
                 mySqlConnection.Close();
@@ -32,34 +37,39 @@ namespace TheNewPanelists.DataAccessLayer
             EstablishMariaDBConnection();
         }
 
-        private void DeleteTempUser() {
-
+        private void DeleteTempUser() 
+        {
             using (mySqlConnection = new MySqlConnection("server=localhost;user=MotoMotoA;password=password;"))
-            using (MySqlCommand cmd = new MySqlCommand("DROP USER 'tempuser'@'localhost'", mySqlConnection)) {
-                try {
+            using (MySqlCommand cmd = new MySqlCommand("DROP USER 'tempuser'@'localhost'", mySqlConnection)) 
+            {
+                try 
+                {
                     mySqlConnection.Open();
                     Console.WriteLine("Connection Open...");
                     cmd.ExecuteNonQuery();
                     Console.WriteLine("Temp User Dropped...");
-                } catch (Exception e) {
+                }
+                catch (Exception e) 
+                {
                     Console.WriteLine("Exited Program with Exit "+e);
                 }
                 mySqlConnection.Close();
             }
-            
         }
-        public bool EstablishMariaDBConnection()
-        {
-            
+        public bool EstablishMariaDBConnection() 
+        {    
             // NOTE: hardcoded, will be different based on your naming
             string connectionString = "server=localhost;user=MotoMotoA;database=motomotousermanagement;port=3306;password=password";
             // @"Data Source=localhost;User ID=admin_MM_test;Password=l23";
 
-            try {
+            try 
+            {
                 mySqlConnection = new MySqlConnection(connectionString);
                 mySqlConnection.Open();
                 Console.WriteLine("Connection open");
-            } catch (Exception e) {
+            } 
+            catch (Exception e) 
+            {
                 Console.WriteLine("Invalid Connection "+e+ " creating temp user now");
                 this.BuildTempUser();
             }
@@ -74,25 +84,42 @@ namespace TheNewPanelists.DataAccessLayer
 
         public string SqlGenerator()
         {   
-            return "";
+            if (this.operation == "FIND") 
+            {
+                return this.FindUser();
+            }
+            else if (this.operation == "CREATE") 
+            {
+                return this.CreateUser();
+            } 
+            else if (this.operation == "DROP") 
+            {
+                return this.DropUser();
+            }
+            else if (this.operation == "UPDATE") 
+            {
+                return this.UpdateUser();
+            }
         }
-
-        private string FindUser() {
+        private string FindUser() 
+        {
             return "SELECT u FROM User u WHERE u.username ="+this.userInfo["username"]+";";
         }
 
-        private string CreateUser() {
+        private string CreateUser() 
+        {
             return "INSERT INTO USER VALUES (NULL, NULL, "+this.userInfo["username"]+", "
                    +this.userInfo["password"]+", "+this.userInfo["email"]+", false, false);";
         }
 
-        private string DropUser() {
+        private string DropUser() 
+        {
             return "DELETE u FROM USER u WHERE u.username ="+this.userInfo["username"]+";";
         }
 
-        private string UpdateUser() {
-            return "UPDATE USER u SET u.username = "+this.userInfo["newusername"]+" WHERE u.username="
-                    +this.userInfo["username"];
+        private string Update() 
+        {
+            return $@"UPDATE USER u SET u.{ this.userInfo[1].key } = { this.userInfo[1].value } WHERE u.username={ this.userInfo[0] }";
         }
     }
 }
