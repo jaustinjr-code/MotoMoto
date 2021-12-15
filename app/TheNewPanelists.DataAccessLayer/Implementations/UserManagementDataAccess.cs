f9b1044c726d832f9839ecd6a789b22be549bcd6
Jacob Nagano-Sunia <jacob.sunia@student.csulb.edu>
2021-12-15 03:11:58 -0800

    Updated Files for DA and SL

--- app/TheNewPanelists.DataAccessLayer/Implementations/UserManagementDataAccess.cs
+++ app/TheNewPanelists.DataAccessLayer/Implementations/UserManagementDataAccess.cs
@@ -1,71 +1,83 @@
 using System.Linq;
 using MySql.Data.MySqlClient;
+using System.Collections;
+using System.Text;
 
 
 namespace TheNewPanelists.DataAccessLayer
 {
     class UserManagementDataAccess : IDataAccess
     {
-        private Dictionary<string, string> userInfo { get; set; }
-        private string operation { get; set; }
+        private string query { get; set; }
         private MySqlConnection mySqlConnection = null;
         public UserManagementDataAccess()
         {
         }
-        public UserManagementDataAccess(string operation, Dictionary<string, string> userInfo)
+        public UserManagementDataAccess(string query)
         {
-            this.userInfo = userInfo;
-            this.operation = operation;
+            this.query = query;
         }
         private void BuildTempUser()
         {
-            using (mySqlConnection = new MySqlConnection("server=localhost;user=MotoMotoA;password=password;"))
-            using (MySqlCommand cmd = new MySqlCommand("CREATE USER 'tempuser'@'localhost' IDENTIFIED BY 'password';", mySqlConnection))
+            // Hides password
+            StringBuilder input = new StringBuilder();
+            while (true)
             {
-                try
-                {
-                    mySqlConnection.Open();
-                    Console.WriteLine("Connection Open...");
-                    cmd.ExecuteNonQuery();
-                    Console.WriteLine("Temp User Created...");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Exited Program with Exit " + e);
-                }
-                mySqlConnection.Close();
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter) break;
+                if (key.Key == ConsoleKey.Backspace && input.Length > 0) input.Remove(input.Length - 1, 1);
+                else if (key.Key != ConsoleKey.Backspace) input.Append(key.KeyChar);
+            }
+            string pass = input.ToString();
+            string user = System.Environment.UserName;
+
+
+            Console.WriteLine(pass);
+            Console.WriteLine(System.Environment.UserName);
+
+            MySqlConnection tempMySqlConnection = new MySqlConnection($"server=localhost;user=root;password={pass}");
+            // MySqlConnection tempMySqlConnection = new MySqlConnection($"server=localhost;user={user};password={pass}");
+            try
+            {
+                tempMySqlConnection.Open();
+                // MySqlCommand cmd1 = new MySqlCommand("DROP USER IF EXISTS 'tempuser'@'localhost';", tempMySqlConnection);
+                MySqlCommand cmd2 = new MySqlCommand("CREATE USER IF NOT EXISTS 'tempuser'@'localhost' IDENTIFIED BY '123';", tempMySqlConnection);
+                MySqlCommand cmd3 = new MySqlCommand("GRANT ALL PRIVILEGES ON *.* TO 'tempuser'@'localhost' WITH GRANT OPTION;", tempMySqlConnection);
+                MySqlCommand cmd4 = new MySqlCommand("FLUSH PRIVILEGES;", tempMySqlConnection);
+                // MySqlCommand cmd4 = new MySqlCommand("SHOW DATABASE LIKE logs_MM_test;", tempMySqlConnection);
+                // MySqlCommand cmd5 = new MySqlCommand("CREATE DATABASE IF NOT EXISTS logs_MM_test;", tempMySqlConnection);
+
+                Console.WriteLine("Connection Open...");
+                // cmd1.ExecuteNonQuery();
+                Console.WriteLine("DROP");
+                cmd2.ExecuteNonQuery();
+                Console.WriteLine("GRANT");
+                cmd3.ExecuteNonQuery();
+                Console.WriteLine("FLUSH");
+                cmd4.ExecuteNonQuery();
+                Console.WriteLine("CREATE");
+                // cmd5.ExecuteNonQuery();
+                Console.WriteLine("Temp User Created...");
+                tempMySqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exited Program with Exit " + e.Message);
             }
             EstablishMariaDBConnection();
-        }
-        private void DeleteTempUser()
-        {
-            using (mySqlConnection = new MySqlConnection("server=localhost;user=MotoMotoA;password=password;"))
-            using (MySqlCommand cmd = new MySqlCommand("DROP USER 'tempuser'@'localhost'", mySqlConnection))
-            {
-                try
-                {
-                    mySqlConnection.Open();
-                    Console.WriteLine("Connection Open...");
-                    cmd.ExecuteNonQuery();
-                    Console.WriteLine("Temp User Dropped...");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Exited Program with Exit " + e);
-                }
-                mySqlConnection.Close();
-            }
         }
         public bool EstablishMariaDBConnection()
         {
             // NOTE: hardcoded, will be different based on your naming
-            string connectionString = "server=localhost;user=MotoMotoA;database=motomotousermanagement;port=3306;password=password";
+            string connectionString = "server=localhost;user=tempuser;database=motomotousermanagement;port=3306;password=123;";
             // @"Data Source=localhost;User ID=admin_MM_test;Password=l23";
+            mySqlConnection = new MySqlConnection(connectionString);
 
             try
-            {
-                mySqlConnection = new MySqlConnection(connectionString);
+            {   
                 mySqlConnection.Open();
+                MySqlCommand command = new MySqlCommand(this.query, mySqlConnection);
+                command.ExecuteNonQuery();
                 Console.WriteLine("Connection open");
             }
             catch (Exception e)
@@ -78,43 +90,19 @@ namespace TheNewPanelists.DataAccessLayer
             mySqlConnection.Close();
             return false;
         }
-        public string SqlGenerator()
+        public bool SelectAccount()
         {
-            if (this.operation == "FIND")
+            if (!EstablishMariaDBConnection()) Console.WriteLine("Connection failed to open...");
+            else Console.WriteLine("Connection opened...");
+
+            MySqlCommand command = new MySqlCommand(this.query, mySqlConnection);
+            if (command.ExecuteNonQuery() == 1)
             {
-                return this.FindUser();
+                mySqlConnection.Close();
+                Console.WriteLine("Connection closed...");
+                return true;
             }
-            else if (this.operation == "CREATE")
-            {
-                return this.CreateUser();
-            }
-            else if (this.operation == "DROP")
-            {
-                return this.DropUser();
-            }
-            else if (this.operation == "UPDATE")
-            {
-                return this.Update();
-            }
-            return "";
-        }
-        private string FindUser()
-        {
-            return "SELECT u FROM User u WHERE u.username =" + this.userInfo["username"] + ";";
-        }
-        private string CreateUser()
-        {
-            return "INSERT INTO USER VALUES (NULL, NULL, " + this.userInfo["username"] + ", "
-                   + this.userInfo["password"] + ", " + this.userInfo["email"] + ", false, false);";
-        }
-        private string DropUser()
-        {
-            return "DELETE u FROM USER u WHERE u.username =" + this.userInfo["username"] + ";";
-        }
-        private string Update()
-        {
-            return "UPDATE USER u SET u.username = " + this.userInfo["newusername"] +
-                    " WHERE u.username=" + this.userInfo["username"];
+            return false;
         }
     }
 }
