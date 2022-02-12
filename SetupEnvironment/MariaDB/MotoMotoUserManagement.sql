3fabf8f498a97e74a6ec020559ac652534a9f67a
Jacob Nagano-Sunia <jacob.sunia@student.csulb.edu>
2022-02-11 19:28:17 -0800

    UserManagement User and Profile modified

--- SetupEnvironment/MariaDB/MotoMotoUserManagement.sql
+++ SetupEnvironment/MariaDB/MotoMotoUserManagement.sql
@@ -4,20 +4,29 @@ CREATE TABLE Type (
     CONSTRAINT Type_PK PRIMARY KEY (typeId),
     CONSTRAINT Type_UK UNIQUE (typeName)
 );
-CREATE TABLE User (
+
+CREATE TABLE Profile (
     typeId INT NOT NULL,
     userId INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(25) NOT NULL,
-    password VARCHAR(25) NOT NULL,
-    email VARCHAR(50) NOT NULL,
     status BOOL NOT NULL,
     eventAccount BOOL NOT NULL,
     CONSTRAINT User_PK PRIMARY KEY (userId),
     CONSTRAINT Username_UK UNIQUE (username),
-    CONSTRAINT Email_UK UNIQUE (email),
     CONSTRAINT User_Type_FK FOREIGN KEY (typeId) REFERENCES Type (typeId)
 );
+
+CREATE TABLE User (
+	userID  INT NOT NULL,
+	username  VARCHAR(50) NOT NULL, 
+	password  VARCHAR(50) NOT NULL,
+	email  VARCHAR(100) NOT NULL, 
+    CONSTRAINT profile_Pk PRIMARY KEY (userID, username),
+	CONSTRAINT userID_fk FOREIGN KEY (userId) REFERENCES Profile (userId)
+);
+
 INSERT INTO Type
 VALUES (NULL, 'ADMIN'),
     (NULL, 'REGISTERED'),
     (NULL, "DEFAULT");
+
