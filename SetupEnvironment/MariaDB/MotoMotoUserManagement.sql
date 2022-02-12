394a485a68c1813ee77706f5564f44eefab99150
Jacob Nagano-Sunia <jacob.sunia@student.csulb.edu>
2022-02-11 20:23:02 -0800

    Update User and Profile

--- SetupEnvironment/MariaDB/MotoMotoUserManagement.sql
+++ SetupEnvironment/MariaDB/MotoMotoUserManagement.sql
@@ -1,28 +1,28 @@
 CREATE TABLE Type (
     typeId INT NOT NULL AUTO_INCREMENT,
     typeName VARCHAR(25) NOT NULL,
-    CONSTRAINT Type_PK PRIMARY KEY (typeId),
-    CONSTRAINT Type_UK UNIQUE (typeName)
-);
-
-CREATE TABLE Profile (
-    typeId INT NOT NULL,
-    userId INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(25) NOT NULL,
-    status BOOL NOT NULL,
-    eventAccount BOOL NOT NULL,
-    CONSTRAINT User_PK PRIMARY KEY (userId),
-    CONSTRAINT Username_UK UNIQUE (username),
-    CONSTRAINT User_Type_FK FOREIGN KEY (typeId) REFERENCES Type (typeId)
+    CONSTRAINT Type_PK PRIMARY KEY (typeName),
+    CONSTRAINT Type_UK UNIQUE (typeId)
 );
 
 CREATE TABLE User (
-	userID  INT NOT NULL,
-	username  VARCHAR(50) NOT NULL, 
+    typeName VARCHAR(25) NOT NULL,
+	userId INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(25) NOT NULL,
 	password  VARCHAR(50) NOT NULL,
 	email  VARCHAR(100) NOT NULL, 
-    CONSTRAINT profile_Pk PRIMARY KEY (userID, username),
-	CONSTRAINT userID_fk FOREIGN KEY (userId) REFERENCES Profile (userId)
+    status BOOL NOT NULL,
+    CONSTRAINT user_Pk PRIMARY KEY (userId, username),
+    CONSTRAINT User_Type_FK FOREIGN KEY (typeName) REFERENCES Type (typeName)
+);
+
+CREATE TABLE Profile (
+    userId INT NOT NULL,
+    username VARCHAR(25) NOT NULL,
+    eventAccount BOOL NOT NULL,
+    CONSTRAINT Profile_Pk PRIMARY KEY (username),
+    CONSTRAINT Username_UK UNIQUE (username),
+    CONSTRAINT User_ID_FK FOREIGN KEY (userId, username) REFERENCES User (userId, username)
 );
 
 INSERT INTO Type
