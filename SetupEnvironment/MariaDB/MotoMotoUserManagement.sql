b18db226f117cb77f87ebb940361a66e4d36dd94
Jacob Nagano-Sunia <70661851+TakeshiJay@users.noreply.github.com>
2022-02-23 19:46:14 -0800

    updated UM Tables

--- SetupEnvironment/MariaDB/MotoMotoUserManagement.sql
+++ SetupEnvironment/MariaDB/MotoMotoUserManagement.sql
@@ -6,25 +6,26 @@ CREATE TABLE Type (
 );
 
 CREATE TABLE User (
-    typeName VARCHAR(25) NOT NULL,
-	userId INT NOT NULL AUTO_INCREMENT,
+-- should add typeID that references profile table
+    userId INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(25) NOT NULL,
-	password  VARCHAR(50) NOT NULL,
-	email  VARCHAR(100) NOT NULL, 
-    status BOOL NOT NULL,
-    CONSTRAINT user_Pk PRIMARY KEY (userId, username),
-    CONSTRAINT userEmail_Uk UNIQUE (email),
-    CONSTRAINT User_Type_FK FOREIGN KEY (typeName) REFERENCES Type (typeName)
+    password  VARCHAR(50) NOT NULL,
+    email  VARCHAR(100) NOT NULL, 
+    CONSTRAINT user_Pk PRIMARY KEY (userId)
 );
 
+
 CREATE TABLE Profile (
+    typeId INT NOT NULL,
     userId INT NOT NULL,
     username VARCHAR(25) NOT NULL,
+    status BOOL NOT NULL,
     eventAccount BOOL NOT NULL,
     CONSTRAINT Profile_Pk PRIMARY KEY (username),
     CONSTRAINT Username_UK UNIQUE (username),
-    CONSTRAINT User_ID_FK FOREIGN KEY (userId, username) REFERENCES User (userId, username)
+    CONSTRAINT User_ID_FK FOREIGN KEY (userId) REFERENCES User (userId)
 );
+
 
 INSERT INTO Type
 VALUES (NULL, 'ADMIN'),
