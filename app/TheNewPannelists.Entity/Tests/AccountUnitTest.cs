5e05e585f2e9eacac7d34b3e747557d51a91e8e1
Jacob Nagano-Sunia <70661851+TakeshiJay@users.noreply.github.com>
2022-03-01 13:02:58 -0800

    pushed new account and account unit testing

---
+++ app/TheNewPannelists.Entity/Tests/AccountUnitTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TheNewPanelists.Entity
+{
+    public class AccountTest
+    {
+        [Fact]
+        public void IsValidAccount_WithValidAccount_ReturnTrue()
+        {
+            
+        } 
+    }    
+}
