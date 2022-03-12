4f88e643cbe4a92154af7e876bd03eaac7887cd4
bobdidley <james.austin@student.csulb.edu>
2022-03-12 08:20:36 -0800

    Usage Analysis Dashboard Front End Views

---
+++ frontend/src/components/UsageAnalysisDashboard/UADCommunityBoardChart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>Community Board</h3>
+    <button @click="pressed">Press</button>
+    <p>{{ table }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    pressed: function() {
+      // console.log("NOo")
+            fetch("https://localhost:7197/UsageAnalysisDashboard/GetAnalytics")
+            .then((response) => response.json())
+            .then((d) => {
+              this.table = d;
+            });
+    }
+  },
+  data() {
+      return {
+          table: "Feed Data"
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/UsageAnalysisDashboard/UADEventListChart.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <h3>Event List</h3>
+  <button @click="pressed">Press</button>
+  <p>{{ table }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    pressed: function() {
+      // console.log("NOo")
+            fetch("https://localhost:7197/UsageAnalysisDashboard/GetAnalytics")
+            .then((response) => response.json())
+            .then((d) => {
+              this.table = d;
+            });
+    }
+  },
+  data() {
+      return {
+          table: "Event Data"
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
