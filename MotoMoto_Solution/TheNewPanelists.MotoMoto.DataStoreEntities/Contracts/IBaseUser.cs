7c7dedf534815ccd5a2dd6d715731f0ac1ad8fd2
bobdidley <james.austin@student.csulb.edu>
2022-03-17 21:22:53 -0700

    DataStoreEntities namespace change

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.DataStoreEntities/Contracts/IBaseUser.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.DataStoreEntities/Contracts/IBaseUser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheNewPanelists.App.DataStoreEntities
+namespace TheNewPanelists.MotoMoto.DataStoreEntities
 {
     public abstract class IBaseUser
     {
