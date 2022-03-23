b5eabdbbf73eaca4fd22b99f4e06defd094fea1b
bobdidley <james.austin@student.csulb.edu>
2022-03-23 15:26:14 -0700

    Models folder and files copied over with new names

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/UserManagementModels/ProfileModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheNewPanelists.MotoMoto.DataStoreEntities;
+
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class ProfileModel
+    {
+        public string? username { get; set; }
+        public bool status { get; set; }
+        public bool eventAccount { get; set; }
+    }
+}
