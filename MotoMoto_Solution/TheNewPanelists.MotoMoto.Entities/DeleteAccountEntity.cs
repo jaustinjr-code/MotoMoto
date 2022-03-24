716f57eedf10d7e5f213ceff3d194e9863045a95
Jacob Nagano-Sunia <70661851+TakeshiJay@users.noreply.github.com>
2022-03-24 12:41:54 -0700

    Revert "Removed entities"
    This reverts commit b495e120dad81ad49005ba24899bb9f302888d41.

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Entities/DeleteAccountEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class DeleteAccountEntity
+    {
+        public string? username { get; set; }
+        public string? verifiedPassword { get; set; }
+    }
+}
---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Entities/EntityType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheNewPanelists.MotoMoto.DataStoreEntities;
+
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class EntityType
+    {
+        public int _typeId { get; set; }
+        public string? _typeName { get; set; }
+    }
+}
