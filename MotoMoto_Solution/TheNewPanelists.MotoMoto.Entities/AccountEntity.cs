9873094150e8fb5c31a09c7869268fbddd331421
Jacob Nagano-Sunia <70661851+TakeshiJay@users.noreply.github.com>
2022-03-24 12:46:57 -0700

    Revert "Removed redundant files and will prep to remove redundancies from the csproj file"
    This reverts commit 4a4addda0363fb8e0b8008a65dce96aba4715bc8.

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Entities/AccountEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheNewPanelists.MotoMoto.DataStoreEntities;
+
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class AccountEntity
+    {
+        public string? AccountType { get; set; }
+        public string? username { get; set; }
+
+    }
+}
