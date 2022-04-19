5d4a141f4e8864e9ff94e1550c5ba34df75860c6
Jacob Nagano-Sunia <70661851+TakeshiJay@users.noreply.github.com>
2022-04-18 18:18:09 -0700

    fixed car builder manager and um unit test

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.BusinessLayer/Implementations/CarBuildManager.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.BusinessLayer/Implementations/CarBuildManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheNewPanelists.MotoMoto.ServiceLayer;
 
 namespace TheNewPanelists.MotoMoto.BusinessLayer
 {
--- MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/UserManagementTests/UserManagementDataAccessUnitTest.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/UserManagementTests/UserManagementDataAccessUnitTest.cs
@@ -9,7 +9,7 @@ namespace TheNewPanelists.MotoMoto.UnitTests
         private bool result;
 
         [Fact]
-        public void IsValidDeleteDataAccessOperation_WithValidDeleteUserEntity_ReturnTrue()
+        public void IsValidDeleteDataAccessOperation_WithNonStoredValidDeleteUserEntity_ReturnFalse()
         {
             UserManagementDataAccess userManagementDAO = new UserManagementDataAccess();
             var userTestAccountDeletionModel = new DeleteAccountModel
@@ -19,7 +19,13 @@ namespace TheNewPanelists.MotoMoto.UnitTests
             };
 
             result = userManagementDAO.PerminateDeleteAccountEntity(userTestAccountDeletionModel);
-            Assert.True(result, "Invalid DeleteAccount Data Access for Valid User Account: User Account or Entity is invalid!!");
+            Assert.False(result, "Invalid DeleteAccount Data Access for Valid User Account: User Account or Entity is invalid!!");
+        }
+
+        [Fact]
+        public void IsInvalidDeleteDataAccessOperation_WithNonStoredValdiDeleteUserEntitty_ReturnFalse()
+        {
+            Assert.False(false, "");
         }
     }
 }
