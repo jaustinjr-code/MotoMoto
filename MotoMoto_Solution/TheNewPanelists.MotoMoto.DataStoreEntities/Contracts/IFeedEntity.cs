9b76f89f5eb84af7aa440962e8ca2580b691e75c
bobdidley <james.austin@student.csulb.edu>
2022-04-20 00:14:29 -0700

    Fixed Community Board related warnings and errors

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.DataStoreEntities/Contracts/IFeedEntity.cs
@@ -0,0 +1,9 @@
+namespace TheNewPanelists.MotoMoto.DataStoreEntities
+{
+    public interface IFeedEntity
+    {
+        string feedName { get; }
+        IEnumerable<IPostEntity> postList { get; }
+        IAnalytic feedAnalytic { get; set; }
+    }
+}
---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/CommunityBoardModels/Implementations/CommunityFeedModel.cs
@@ -0,0 +1,8 @@
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class CommunityFeedModel : IFeedModel
+    {
+        public string feedName { get; set; } = default!;
+        public IEnumerable<IPostModel> postList { get; set; } = default!;
+    }
+}
