e34a14f6a52d4de14c16f1941120cfe524166406
bobdidley <james.austin@student.csulb.edu>
2022-04-22 22:31:32 -0700

    CB: Update Post Models to Nullable values

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/CommunityBoardModels/Implementations/FeedPostModel.cs
@@ -0,0 +1,27 @@
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class FeedPostModel : IPostModel
+    {
+        public int postID { get; }
+        public string? postTitle { get; set; }
+        public string? contentType { get; set; }
+        public string? postUser { get; set; }
+        public string? postDescription { get; set; }
+        public DateTime submitUTC { get; set; }
+        public IEnumerable<byte[]>? imageList { get; set; }
+
+        public FeedPostModel(int id)
+        {
+            postID = id;
+        }
+
+        public FeedPostModel(int id, string title, string type, string username, DateTime utc)
+        {
+            postID = id;
+            postTitle = title;
+            contentType = type;
+            postUser = username;
+            submitUTC = utc;
+        }
+    }
+}
