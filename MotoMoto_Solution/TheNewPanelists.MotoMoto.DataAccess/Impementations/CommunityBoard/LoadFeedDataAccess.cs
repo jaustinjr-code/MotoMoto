9ca95bedfcd2d4b5cdf55b089ecc88150d4e5a73
bobdidley <james.austin@student.csulb.edu>
2022-04-23 00:46:13 -0700

    CB: LoadFeedDataAccess and Model comments

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.DataAccess/Impementations/CommunityBoard/LoadFeedDataAccess.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.DataAccess/Impementations/CommunityBoard/LoadFeedDataAccess.cs
@@ -12,12 +12,18 @@ namespace TheNewPanelists.MotoMoto.DataAccess
         //private Object? _input;
         //MySqlCommand? _command;
 
+        // Default Empty Constructor
+        // I opted out of using this because I may not need to define _connectionString
+        // or create a _mySqlConnection if something goes wrong in the process of the operation.
         public LoadFeedDataAccess() { }
 
+        // Establishes the MariaDB connection for cleaner code
+        // I try to use the ConfigurationManager to avoid hardcoding the connection
+        // but it does not work
         public bool EstablishMariaDBConnection()
         {
             // Production code
-            // ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["MotoMotoRDSConnection"];
+            //ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["MotoMotoRDSConnection"];
             // Development code
             ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["MotoMotoDevDBConnection"];
             if (settings != null)
@@ -30,10 +36,14 @@ namespace TheNewPanelists.MotoMoto.DataAccess
             //return false;
             //}
 
+            // NOTE: this is not ideal and should be assigned using .config file attributes
+            //       but the ConfigurationManager always returns null and I don't know why
             _connectionString = "server=localhost;user=dev_moto;database=dev_UM;port=3306;password=motomoto;";
 
+            // Creates a SqlConnection and opens it
             try
             {
+                // Close the connection if one is already opened
                 if (_mySqlConnection != null && _mySqlConnection.State == System.Data.ConnectionState.Open)
                     _mySqlConnection.Close();
 
@@ -58,12 +68,19 @@ namespace TheNewPanelists.MotoMoto.DataAccess
         //return "";
         //}
 
+        // Refines the data retrieved by the MySqlDataReader
         public IEnumerable<IPostEntity> RefineData(MySqlDataReader reader)
         {
+            // I opted of using a for-loop with FieldCount b/c I know what I want from the record
+            // Although the database can be altered in the future, the hassle of doing it makes it
+            // unlikely that these changes would be done often, but it is still not robust in my opinion
             //int col = reader.FieldCount;
+
+            // Checks if there are any records to refine
             if (reader.HasRows)
             {
                 IEnumerable<IPostEntity> postList = new List<IPostEntity>();
+                // Continue to read until there are no more records
                 while (reader.Read())
                 {
                     int postId = reader.GetInt32("postId");
@@ -73,6 +90,7 @@ namespace TheNewPanelists.MotoMoto.DataAccess
                     // Requires MySqlDataReader on Images table
                     //IEnumerable<byte[]> imageList = reader.GetByte("imageList");
                     IPostEntity post = new DataStorePost(postId, postTitle, postUsername, postDescription, null);
+                    // Append from IEnumerable does not work so must use a List object casting for Add
                     ((List<IPostEntity>)postList).Add(post);
                     //Console.WriteLine(post);
                     //Console.WriteLine(post.postId + " " + post.postTitle);
@@ -89,6 +107,10 @@ namespace TheNewPanelists.MotoMoto.DataAccess
         //return command;
         //}
 
+        // Fetches only one post according to the post ID
+        // The post ID will be available on the client side because it is the only unqiue attribute
+        // for a post at the moment
+        // As long as the post ID is not modifiable throughout this process then nothing should break
         public IPostEntity? FetchPost(IPostModel postInput)
         {
             if (!EstablishMariaDBConnection())
@@ -96,13 +118,15 @@ namespace TheNewPanelists.MotoMoto.DataAccess
                 throw new NullReferenceException();
             }
 
-            // The post id will be visible on the client side so using this info is okay
+            // The post ID will be visible on the client side so using this info is okay
             string commandText = "SELECT * FROM Post WHERE postID = @postID;";
             using (MySqlCommand command = new MySqlCommand(commandText, _mySqlConnection))
             {
                 command.Parameters.AddWithValue("@postID", postInput.postID);
                 try
                 {
+                    // _mySqlConnection must not be null when beginning the transaction
+                    // Beginning the transaction will improve finding bugs in data integrity
                     _mySqlConnection!.BeginTransaction();
                     IPostEntity result = ((List<IPostEntity>)RefineData(command.ExecuteReader())).ElementAt(0);
                     _mySqlConnection.Close();
@@ -119,6 +143,8 @@ namespace TheNewPanelists.MotoMoto.DataAccess
             return entity;
         }
 
+        // Fetches all the posts of a specified community feed name
+        // Feed names are unique, that's why it is input for this operation
         public IFeedEntity? FetchAllPosts(IFeedModel feedInput)
         {
             // Establish connection assigns connection string
@@ -129,11 +155,12 @@ namespace TheNewPanelists.MotoMoto.DataAccess
             //else _mySqlConnection!.BeginTransaction();
 
             // Select most recent posts within set number of days
+            // Order by the most recent to the latest because that is how the feed
+            // should be organized
             string commandText = "SELECT * FROM Post WHERE feedName LIKE @feedname AND DATEDIFF(NOW(), submitUTC) < @diff ORDER BY submitUTC DESC;";
             using (MySqlCommand command = new MySqlCommand(commandText, _mySqlConnection))
             {
-                //MySqlParameter[] parameters = new MySqlParameter[1];
-                //parameters[0] = new MySqlParameter("@feedname", feedInput.feedName);
+                // Difference of NOW and the post UTC submission time is a week
                 int diff = 7;
                 command.Parameters.AddWithValue("@feedname", feedInput.feedName);
                 command.Parameters.AddWithValue("@diff", diff);
@@ -143,6 +170,9 @@ namespace TheNewPanelists.MotoMoto.DataAccess
                     command.CommandTimeout = TimeSpan.FromSeconds(60).Seconds;
                     IEnumerable<IPostEntity> result = RefineData(command.ExecuteReader());
                     _mySqlConnection.Close();
+
+                    // Defining the DataStoreCommunityFeed is unrelated to the SqlCommand so it is
+                    // past the Close statement
                     IFeedEntity feed = new DataStoreCommunityFeed(feedInput.feedName, result, null);
                     //Console.WriteLine(feed.feedName + " " + feed.postList.ElementAt(0).postTitle);
                     //Console.WriteLine(feed);
@@ -154,6 +184,8 @@ namespace TheNewPanelists.MotoMoto.DataAccess
                     e.ToString();
                 }
             }
+            // This should return null if no posts were found, but an alternative is
+            // to continue to query the next posts in recency upon the user's request
             return null;
 
             // Connect to MariaDB
