30f4a9c22506e58a03e3b7ca7aced8b08e139aba
JustinCalma <44331820+JustinCalma@users.noreply.github.com>
2022-04-24 21:54:55 -0700

    Created models for Event List Feed and Post

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/EventListModels/Implementations/EventFeedModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class EventFeedModel : IFeedModel
+    {
+        public string feedName { get; set; } = default!;
+        public IEnumerable<IPostModel> postList { get; set; } = default!;
+    }
+}
---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/EventListModels/Implementations/EventPostModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class EventPostModel : IPostModel
+    {
+        public int postID { get; }
+        public string? postTitle { get; set; }
+        public string? contentType { get; set; }
+        public string? postUser { get; set; }
+        public string? postDescription { get; set; }
+        public DateTime submitUTC { get; set; }
+        public IEnumerable<byte[]>? imageList { get; set; }
+
+        public EventPostModel(int id)
+        {
+            postID = id;
+        }
+
+        public EventPostModel(int id, string title, string type, string username, DateTime utc)
+        {
+            postID = id;
+            postTitle = title;
+            contentType = type;
+            postUser = username;
+            submitUTC = utc;
+        }
+    }
+}
