2bfdf5fc86bcc01db7fa4760f75ac004c4825146
JustinCalma <44331820+JustinCalma@users.noreply.github.com>
2022-04-25 09:07:58 -0700

    Create Event List Controller in the WebService for Event List

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.WebServices.EventList/Controllers/EventListController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheNewPanelists.MotoMoto.WebServices.EventList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventListController : Controller
+    {
+        [HttpOptions]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
