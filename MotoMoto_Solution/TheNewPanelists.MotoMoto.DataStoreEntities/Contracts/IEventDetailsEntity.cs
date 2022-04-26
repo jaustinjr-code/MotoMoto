665488513bdb61561f5024868d4984225c2016db
JustinCalma <calma.justin.00@gmail.com>
2022-04-26 15:02:52 -0700

    Create Interface and Implementation for Event List model

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.DataStoreEntities/Contracts/IEventDetailsEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheNewPanelists.MotoMoto.DataStoreEntities
+{
+    // IEventDetailsEntity implements IPostEntity and stores details regarding events
+    public interface IEventDetailsEntity : IPostEntity
+    {
+        int? eventID { get; }
+        string? eventLocation { get; }
+        string? eventTime { get; }
+        string? eventDate { get; }
+        string? registeredUsers { get; }
+    }
+}
