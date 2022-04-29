ddf33fbaf3ecd3016e3e536c9e40da0cd6e72dec
Blake Del Rey <bcdelrey@gmail.com>
2022-04-28 19:40:44 -0700

    Part Flagging: Add production url as base url in frontend
    production url as base url in frontend

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/PartFlaggingConnection.js
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/PartFlaggingConnection.js
@@ -4,7 +4,8 @@ axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'DELETE, POST, GET, OPTIONS';
 axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With';
 const instance = axios.create({
-    baseURL: 'https://localhost:7285/api/'
+    // baseURL: 'https://localhost:7285/api/'
+    baseURL: 'http://motomotoca.com:5010/api/'
 });
 
 export {instance}
