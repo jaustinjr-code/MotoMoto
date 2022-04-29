0293f54bed00cfc8ba994584a8ed06602f9c6e40
bobdidley <james.austin@student.csulb.edu>
2022-04-28 20:03:02 -0700

    CB: Feed View started

---
+++ MotoMoto_Solution/thenewpanelists.motomoto.frontend/src/views/CommunityBoardViews/FeedView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <feed-community-dropdown-component></feed-community-dropdown-component>
+        <FeedPostsComponent/>
+    </div>
+</template>
+
+<script>
+import FeedCommunityDropdownComponent from '../../components/CommunityBoardComponents/FeedComponents/FeedCommunityDropdownComponent.vue'
+import FeedPostsComponent from '../../components/CommunityBoardComponents/FeedComponents/FeedPostsComponent.vue'
+export default {
+    components: { 
+        FeedCommunityDropdownComponent, 
+        FeedPostsComponent
+    },
+}
+</script>
