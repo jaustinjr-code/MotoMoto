677301cc4b6f901a1c2b35c524b7615c95e680de
Jacob Nagano-Sunia <70661851+TakeshiJay@users.noreply.github.com>
2022-04-28 23:44:26 -0700

    Revert "rm homeview to avoid merge conf"
    This reverts commit e1d5cdff72b6c46e628f65e4205d2f462f74277a.

---
+++ MotoMoto_Solution/thenewpanelists.motomoto.frontend/src/views/HomeView.vue
@@ -0,0 +1,35 @@
+import http from "./Http-coman"
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to MotoMoto!"/>
+    <button @click = "goToLogin"> Login </button>
+    <LoginComponent/>
+    <button @click = "goToEventList"> Event List </button>
+    <EventListDashboardComponent/>
+    <button @click = "goToRegistration"> Register </button>
+    <RegistrationComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  methods:{
+    goToLogin(){
+      this.$router.push('/Login');
+    },
+    goToEventList(){
+      this.$router.push('/EventList');
+    }, 
+    goToRegistration(){
+      this.$router.push('/Registration')
+    }
+  }
+}
+</script>
