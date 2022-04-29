78ba31987f552d935ebd7198907a46c53badc77f
Blake Del Rey <bcdelrey@gmail.com>
2022-04-28 22:16:04 -0700

    Part Flagging: Changed port number of Web API to match EC2 instance
    Changed port number of Web API to match EC2 instance

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/PartFlaggingConnection.js
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/PartFlaggingConnection.js
@@ -5,7 +5,7 @@ axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'DELETE, POST, G
 axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With';
 const instance = axios.create({
     // baseURL: 'https://localhost:7285/api/'
-    baseURL: 'http://motomotoca.com:5010/api/'
+    baseURL: 'http://motomotoca.com:5555/api/'
 });
 
 export {instance}
