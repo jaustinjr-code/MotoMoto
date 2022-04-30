ed74c1de4d71216755fba20e88154a62f6257eec
bobdidley <james.austin@student.csulb.edu>
2022-04-30 10:31:26 -0700

    CB: created Upvote Comment Model

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/CommunityBoardModels/Implementations/UpvoteCommentModel.cs
@@ -0,0 +1,17 @@
+namespace TheNewPanelists.MotoMoto.Models
+{
+    public class UpvoteCommentModel : IInteractionModel
+    {
+        public int contentId { get; set; }
+        public int postId { get; set; }
+        public string? contentTitle { get; set; }
+        public string interactUsername { get; set; }
+        public UpvoteCommentModel(int cid, int pid, string title, string username)
+        {
+            contentId = cid;
+            postId = pid;
+            contentTitle = title;
+            interactUsername = username;
+        }
+    }
+}
