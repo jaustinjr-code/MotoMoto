71050a3136c8905c13a315cb902c406fccbcf17f
bobdidley <james.austin@student.csulb.edu>
2022-04-30 13:45:32 -0700

    CB: Updated Service Unit Tests

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/CommunityBoardTests/CommunityBoardServiceLayerUnitTests.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/CommunityBoardTests/CommunityBoardServiceLayerUnitTests.cs
@@ -14,7 +14,7 @@ public class CommunityBoardServiceLayerUnitTests
     // because that would require comparing against dummy data, instead
     // I used System.Diagnostics to write the contents with Debug.WriteLine
     [Fact]
-    public void IsResponseModelBuilt()
+    public void IsResponseModelBuilt_LoadFeed_NotNull()
     {
         // Arrange
         IContentModel content = new CommunityFeedModel();
@@ -31,10 +31,28 @@ public class CommunityBoardServiceLayerUnitTests
     // because that would require comparing against dummy data, instead
     // I used System.Diagnostics to write the contents with Debug.WriteLine
     [Fact]
-    public void IsDefaultResponseModelBuilt()
+    public void IsDefaultResponseModelBuilt_LoadFeed_NotNull()
     {
         // Arrange
         IContentModel content = new CommunityFeedModel();
+        IFetchContentService service = new LoadFeedService(content);
+        // Act
+        IResponseModel result = ((LoadFeedService)service).LoadFeed();
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    // This test checks if the BuildExceptionResponse method builds the response
+    // Useful for analyzing and creating client-friendly exceptions
+    // Exception should come from the specific database operation, for example, FetchAllPosts
+    // If the exception originates from a different operation then something is wrong
+    // NOTE: this test does not check the origin of the Exception
+    [Fact]
+    public void IsExceptionResponseModelBuilt_LoadFeed_NotNull()
+    {
+        // Arrange
+        IContentModel content = new CommunityFeedModel();
+        ((CommunityFeedModel)content).feedName = "unknown";
         IFetchContentService service = new LoadFeedService(content);
         // Act
         IResponseModel result = ((LoadFeedService)service).LoadFeed();
