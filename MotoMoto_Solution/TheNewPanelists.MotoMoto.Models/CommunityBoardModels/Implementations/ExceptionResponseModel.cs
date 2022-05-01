2c79633a2d0728e578b1da3cced070a49b8e2726
bobdidley <james.austin@student.csulb.edu>
2022-04-30 21:05:59 -0700

    CB: updated Exception model

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/CommunityBoardModels/Implementations/ExceptionResponseModel.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/CommunityBoardModels/Implementations/ExceptionResponseModel.cs
@@ -6,12 +6,14 @@ namespace TheNewPanelists.MotoMoto.Models
         public string responseMessage { get; }
         public bool isComplete { get; }
         public bool isSuccess { get; }
+        public Exception exception { get; }
 
         public ExceptionResponseModel(string message)
         {
             responseMessage = message;
             isComplete = false;
             isSuccess = false;
+            exception = new Exception(responseMessage);
         }
     }
 }
