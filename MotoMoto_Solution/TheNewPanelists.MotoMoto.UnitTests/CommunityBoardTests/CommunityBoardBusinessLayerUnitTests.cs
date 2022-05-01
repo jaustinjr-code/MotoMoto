d13135a4dc5ad102199f2535b7d03df9ecb6253a
bobdidley <james.austin@student.csulb.edu>
2022-04-30 21:03:31 -0700

    CB: Post, Comment, Post Details Managers unit tests

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/CommunityBoardTests/CommunityBoardBusinessLayerUnitTests.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/CommunityBoardTests/CommunityBoardBusinessLayerUnitTests.cs
@@ -1,4 +1,6 @@
 using Xunit;
+using System;
+using System.IO;
 using TheNewPanelists.MotoMoto.BusinessLayer;
 using TheNewPanelists.MotoMoto.Models;
 
@@ -13,7 +15,7 @@ namespace TheNewPanelists.MotoMoto.UnitTests.CommunityBoardTests
         }
 
         [Fact]
-        public void IsNullOrEmptyRequestCorrectForNullInput()
+        public void IsNullOrEmptyRequestCorrectForNullInput_LoadFeed_ReturnTrue()
         {
             // Arrange
             object? input = null;
@@ -22,6 +24,95 @@ namespace TheNewPanelists.MotoMoto.UnitTests.CommunityBoardTests
             bool result = manager.IsNullOrEmptyRequest(input);
             // Assert
             Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNullOrEmptyRequestCorrectForEmptyInput_LoadFeed_ReturnTrue()
+        {
+            // Arrange
+            // This should be a request model, not a response
+            IContentModel model = new CommunityFeedModel();
+            ((CommunityFeedModel)((IFeedModel)model)).feedName = "";
+            object? input = new LoadFeedRequestModel(((IFeedModel)model));
+            IContentManager manager = new FeedManager();
+            // Act
+            bool result = manager.IsNullOrEmptyRequest(input);
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNullOrEmptyRequestCorrectForNullInput_Post_ReturnTrue()
+        {
+            // Arrange
+            object? input = null;
+            IContentManager manager = new PostManager();
+            // Act
+            bool result = manager.IsNullOrEmptyRequest(input);
+            // Assert
+            Assert.True(result);
+        }
+
+        // Commented this test because of the warnings
+        // [Fact]
+        // public void IsRequestInputCorrectForNullInput_Post_ReturnFalse()
+        // {
+        //     // Arrange
+        //     IContentModel input = new FeedPostModel(null, null, null, null); // Null inputs give warnings
+        //     IContentManager manager = new PostManager();
+        //     // Act
+        //     bool result = ((PostManager)manager).IsValidRequestForm(input);
+        //     // Assert
+        //     Assert.False(result);
+        // }
+
+        [Fact]
+        public void IsRequestInputCorrectForEmptyInput_Post_ReturnFalse()
+        {
+            // Arrange
+            IContentModel input = new FeedPostModel("", "", "", "");
+            IContentManager manager = new PostManager();
+            // Act
+            bool result = ((PostManager)manager).IsValidRequestForm(input);
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsNullOrEmptyRequestCorrectForNullInput_Comment_ReturnTrue()
+        {
+            // Arrange
+            object? input = null;
+            IContentManager manager = new CommentManager();
+            // Act
+            bool result = manager.IsNullOrEmptyRequest(input);
+            // Assert
+            Assert.True(result);
+        }
+
+        // Commented this test because of the warnings
+        // [Fact]
+        // public void IsRequestInputCorrectForNullInput_Comment_ReturnFalse()
+        // {
+        //     // Arrange
+        //     IContentModel input = new CommentPostModel(0, null, null); // Null inputs give warnings
+        //     IContentManager manager = new CommentManager();
+        //     // Act
+        //     bool result = ((CommentManager)manager).IsValidRequestForm(input);
+        //     // Assert
+        //     Assert.False(result);
+        // }
+
+        [Fact]
+        public void IsRequestInputCorrectForEmptyInput_Comment_ReturnFalse()
+        {
+            // Arrange
+            IContentModel input = new CommentPostModel(0, "", "");
+            IContentManager manager = new CommentManager();
+            // Act
+            bool result = ((CommentManager)manager).IsValidRequestForm(input);
+            // Assert
+            Assert.False(result);
         }
 
         // Not allowed to define null objects for Request model otherwise a warning is thrown
@@ -41,25 +132,32 @@ namespace TheNewPanelists.MotoMoto.UnitTests.CommunityBoardTests
         //Assert.Equal("No Model Found", e.Message);
         //}
 
+
+
         [Fact]
-        public void IsNullOrEmptyRequestCorrectForEmptyInput()
+        public void IsNullOrEmptyRequestCorrectForNullInput_PostDetails_ReturnTrue()
         {
             // Arrange
-            // This should be a request model, not a response
-            IContentModel model = new CommunityFeedModel();
-            ((CommunityFeedModel)((IFeedModel)model)).feedName = "";
-            object? input = new LoadFeedRequestModel(((IFeedModel)model));
-            IContentManager manager = new FeedManager();
+            IRequestModel input = new FetchPostDetailsRequestModel(0);
+            IContentManager manager = new PostDetailsManager();
+
             // Act
-            bool result = manager.IsNullOrEmptyRequest(input);
+            bool result = ((PostDetailsManager)manager).IsNullOrEmptyRequest(input);
+
             // Assert
             Assert.True(result);
         }
 
         [Fact]
-        public void testName()
+        public void IsRequestCorrectForIncorrectInput_PostDetails_ThrowsInvalidDataException()
         {
-            // throw new NotImplementedException();
+            // Given
+            IRequestModel input = new FetchPostDetailsRequestModel(0);
+            IContentManager manager = new PostDetailsManager();
+            // When
+            Exception result = Assert.Throws<InvalidDataException>(() => ((PostDetailsManager)manager).IsRequestValid(input));
+            // Then
+            Assert.NotNull(result);
         }
     }
 }
