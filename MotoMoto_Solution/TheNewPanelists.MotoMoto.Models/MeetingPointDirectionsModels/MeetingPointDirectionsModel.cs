1d6dff0b3572ca167d94a95fa4f6b1c3e3823ece
JustinCalma <44331820+JustinCalma@users.noreply.github.com>
2022-05-02 08:59:01 -0700

    Create Service layer and Model

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/MeetingPointDirectionsModels/MeetingPointDirectionsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheNewPanelists.MotoMoto.DataStoreEntities;
+
+namespace TheNewPanelists.MotoMoto.Models.MeetingPointDirectionsModels
+{
+    public class MeetingPointDirectionsModel : IEventDetailsModel
+    {
+        public int eventID { get; set; }
+        public string? streetAddress { get; set; }
+        public string? city { get; set; }
+        public string? state { get; set; }
+        public string? country { get; set; }
+        public string? zipCode { get; set; }
+    }
+}
