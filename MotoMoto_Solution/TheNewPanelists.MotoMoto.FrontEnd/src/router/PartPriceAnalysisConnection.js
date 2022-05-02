a32a086d136574350fd2f9c97cea6aa4f82763b7
Blake Del Rey <bcdelrey@gmail.com>
2022-05-01 22:10:03 -0700

    Part Price Analysis: Added Correct URL for frontend connection string

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/PartPriceAnalysisConnection.js
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/PartPriceAnalysisConnection.js
@@ -4,7 +4,8 @@ axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'DELETE, POST, GET, OPTIONS';
 axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With';
 const instance = axios.create({
-    baseURL: 'https://localhost:7064/'
+    // baseURL: 'https://localhost:7064/'
+    baseURL: 'http://motomotoca.com:7064/'
 });
 
 export {instance}
