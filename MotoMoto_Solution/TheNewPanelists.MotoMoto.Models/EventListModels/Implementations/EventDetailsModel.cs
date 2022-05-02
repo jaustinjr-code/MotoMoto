55e1f965e7591ec0ad8ba46153217a3cbdf9e8a9
JustinCalma <calma.justin.00@gmail.com>
2022-05-02 11:57:26 -0700

    Modify sql database table and Event Details Model

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/EventListModels/Implementations/EventDetailsModel.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.Models/EventListModels/Implementations/EventDetailsModel.cs
@@ -11,16 +11,16 @@ namespace TheNewPanelists.MotoMoto.Models
     public class EventDetailsModel : IEventDetailsModel
     {
         public int eventID { get; set; }
-        public string? eventLocation { get; set; }
+        //public string? eventLocation { get; set; }
         public string? eventTime { get; set; }
         public string? eventDate { get; set; }
         public IEnumerable<string>? registeredUsers { get; set; }
 
         // USED FOR MEETING POINT DIRECTIONS
-        public string? streetAddress { get; set; }
-        public string? city { get; set; }
-        public string? state { get; set; }
-        public string? country { get; set; }
-        public string? zipCode { get; set; }
+        public string? eventStreetAddress { get; set; }
+        public string? eventCity { get; set; }
+        public string? eventState { get; set; }
+        public string? eventCountry { get; set; }
+        public string? eventZipCode { get; set; }
     }
 }
