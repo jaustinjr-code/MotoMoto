b42f43c4c10bcc14160b3149423326c46457f443
JustinCalma <calma.justin.00@gmail.com>
2022-05-03 14:43:06 -0700

    Prompt users for current location and display latitude and longitude coordinates

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/components/MeetingPointDirectionsComponent.vue
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/components/MeetingPointDirectionsComponent.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>  </div>
+    <div class="form-row justify-content-center align-items-center">
+        <input type="text" class="form-control mb-2" v-model="origin" placeholder="Enter Origin Location">
+        <button type="button" class="btn btn-primary mb-2" @click="findOriginLocation">Search</button>
+    </div>
+    <div class="col-md-6"><h3> Latitude : {{ latitude }}</h3></div>
+    <div class="col-md-6"><h3> Longitude : {{ longitude }}</h3></div>
     <div id="map" ref="map"></div>
 </template>
 
@@ -7,12 +12,34 @@
 import TabBarComponent from './TabBarComponent.vue'
 
 export default {
-    name: 'map',
-    data: () => ({
-        map: null
-    }),
+    name: 'Map',
+    data () {
+        return {
+            origin: '',
+            map: null,
+            latitude: '',
+            longitude: '',
+        }
+    },
     methods: {
-
+        findOriginLocation() {
+            // Determine if browser is supported by geolocation api
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    position => {
+                        this.latitude = position.coords.latitude;
+                        this.longitude = position.coords.longitude;
+                        // console.log(position.coords.latitude);
+                        // console.log(position.coords.longitude);
+                    },
+                    error => {
+                        console.log(error.message);
+                    }
+                );
+            } else {
+                console.log("Your browser does not support geolocatoin API");
+            }
+        }
     },
     mounted() {
         this.map = new window.google.maps.Map(this.$refs["map"], {
@@ -24,15 +51,9 @@ export default {
         //     zoom: 15
         // })
     },
-    methods:{
-
-    },
     components: {
         TabBarComponent
     },
-    methods: {
-
-    }
 }
 </script>
 
