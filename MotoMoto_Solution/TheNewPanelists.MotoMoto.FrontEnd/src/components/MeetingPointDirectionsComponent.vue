75962c8ece3b755ffccb6f86281e02d2260b7e59
JustinCalma <calma.justin.00@gmail.com>
2022-05-05 13:09:06 -0700

    Create 2nd search bar that will be used to autocomplete the destination

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/components/MeetingPointDirectionsComponent.vue
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/components/MeetingPointDirectionsComponent.vue
@@ -2,7 +2,7 @@
     <div class="form-row justify-content-center align-items-center">
         <div class="alert alert-danger" v-show="error">{{ error }}</div>
         <input type="text" class="form-control mb-2" v-model="origin" id="autocomplete" placeholder="Enter Origin Location">
-        <button type="button" class="btn btn-primary mb-2" @click="findOriginLocation">Search</button>
+        <button type="button" class="btn btn-primary mb-2" @click="findOriginLocation">Use Current Location</button>
     </div>
     <!-- <div class="row"> -->
         <div class="col-md-6"><h3> Latitude of Origin : {{ latitude }}</h3></div>
@@ -12,12 +12,26 @@
     <div class="form-row justify-content-center align-items-center">
         <div class="row">
             <h3> Selected Event Location : {{ this.location }}</h3>
+                <input type="text" class="form-control mb-2" v-model="destination" id="autocomplete2" placeholder="Enter Selected Event Location">
             <div class="col-md-6"><h3> Latitude of Event Location : {{ eventLatitude }}</h3></div>
             <div class="col-md-6"><h3> Longitude of Event Location : {{ eventLongitude }}</h3></div>
         </div>
     </div>
 
+    <div class="form-row justify-content-center align-items-center">
+        <div class="row">
+            <h3> Mode of Travel: </h3>
+            <select id="mode">
+                <option value="DRIVING">Driving</option>
+                <option value="WALKING">Walking</option>
+                <option value="BICYCLING">Bicycling</option>
+                <option value="TRANSIT">Transit</option>
+            </select>
+        </div>
+    </div>
     
+    <button type="button" class="btn btn-primary mb-2" @click="findDirections">Find Directions</button>
+
     <!-- <ul>
         <li v-for="(value, index) in id" 
         :key="index" >
@@ -52,6 +66,7 @@ export default {
             eventState: '',
             eventCountry: '',
             eventZipCode: '',
+            destination: '',
         }
     },
     methods: {
@@ -60,15 +75,18 @@ export default {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(
                     position => {
+                        // console.log(postition);
                         this.latitude = position.coords.latitude;
                         this.longitude = position.coords.longitude;
                         this.getAddressFromCoords(this.latitude, this.longitude);
-                        this.showOriginLocationOnMap(this.latitude, this.longitude);
+                        // this.showOriginLocationOnMap(this.latitude, this.longitude);
                         // console.log(position.coords.latitude);
                         // console.log(position.coords.longitude);
                     },
                     error => {
-                        this.error = error.message + " PLEASE TYPE YOUR ADDRESS MANUALLY...";
+                        window.alert(error.message + " REDIRECTING TO EVENT LIST PAGE...");
+                        this.$router.push('/EventList')
+                        // this.error = error.message + " PLEASE TYPE YOUR ADDRESS MANUALLY...";
                         // console.log(error.message);
                     }
                 );
@@ -142,44 +160,98 @@ export default {
                         // this.location = response.data[0].eventStreetAddress;
                         // console.log(this.location);
                         this.eventStreetAddress = response.data[0].eventStreetAddress;
-                        console.log(this.eventStreetAddress);
+                        // console.log(this.eventStreetAddress);
                         
                         this.eventCity = response.data[0].eventCity;
-                        console.log(this.eventCity);
+                        // console.log(this.eventCity);
                         
                         this.eventState = response.data[0].eventState;
-                        console.log(this.eventState);
+                        // console.log(this.eventState);
                         
                         this.eventCountry = response.data[0].eventCountry;
-                        console.log(this.eventCountry);
+                        // console.log(this.eventCountry);
                         
                         this.eventZipCode = response.data[0].eventZipCode;
-                        console.log(this.eventZipCode);
+                        // console.log(this.eventZipCode);
                     
-                        this.location = this.eventStreetAddress + ", " + this.eventCity + ", " + this.eventState + " " + this.eventZipCode;
-                        console.log(this.location);
+                        this.location = this.eventStreetAddress + " " + this.eventCity + " " + this.eventState;// + " " + this.eventZipCode;
+                        // console.log(this.location);
                     }
                 })
             .catch(error => console.log(error))
             .finally(() => console.log('DATA LOADING COMPLETE...'))
         },
+
+        geocodeRetrievedInfo() {
+            console.log("THIS IS IN THE FUNCTION");
+            // var geocoder = new window.google.maps.Geocoder();
+            // geocoder.geocode(this.location);
+
+            var geocoder = new window.google.maps.Geocoder();
+            geocoder.geocode({ address: this.location })
+            .then((response) => {
+                if(response.results[0]) {
+                    this.eventLatitude = response.results[0].lat();
+                    this.eventLongitude = response.results[0].lng();
+                    // console.log(response.results[0].formatted_address);
+                }
+                else {
+                    this.error = response.error_message;
+                    // console.log(response.error_message);
+                }
+            })
+            .catch(error => {
+                this.error = error;
+                // console.log(error.message);
+            })
+
+            // navigator.geolocation.getCurrentPosition(
+            //     position => {
+            //         this.eventLatitude = position.coords.latitude;
+            //         console.log(this.eventLatitude);
+
+            //         this.eventLongitude = position.coords.longitude;
+            //         console.log(this.eventLongitude);
+            //     }
+            // )
+        },
+
+        initMap() {
+            var autocomplete = new window.google.maps.places.Autocomplete(
+                document.getElementById("autocomplete"),
+            )
+            autocomplete.addListener("place_changed", () => {
+                var place = autocomplete.getPlace();
+                // console.log(place);
+                this.origin = place.formatted_address; // MIGHT NOT NEED IF ORIGIN TEXT BOX GETS UPDATED ON CLICK
+                this.latitude = place.geometry.location.lat();
+                this.longitude = place.geometry.location.lng();
+                // this.showOriginLocationOnMap(place.geometry.location.lat(), place.geometry.location.lng());
+            })
+
+            var autocomplete2 = new window.google.maps.places.Autocomplete(
+                document.getElementById("autocomplete2"),
+            )
+            autocomplete2.addListener("place_changed", () => {
+                var place2 = autocomplete2.getPlace();
+                this.destination = place2.formatted_address;
+                this.eventLatitude = place2.geometry.location.lat();
+                this.eventLongitude = place2.geometry.location.lng();
+            })
+        }
     },
     created() {
         this.eventID = this.$route.params.data;
         // console.log(this.eventID);
     },
     async mounted() {
-        var autocomplete = new window.google.maps.places.Autocomplete(
-            document.getElementById("autocomplete"),
-        )
-        autocomplete.addListener("place_changed", () => {
-            var place = autocomplete.getPlace();
-            console.log(place);
-            this.latitude = place.geometry.location.lat();
-            this.longitude = place.geometry.location.lng();
-            this.showOriginLocationOnMap(place.geometry.location.lat(), place.geometry.location.lng());
-        })
+
+        this.initMap();
+
         this.fetchInformation();
+
+        // this.geocodeRetrievedInfo();
+
         // instance.get('MeetingPointDirections/GetEventLocation', {params: { eventID: 2 } })
         //     // .then(response => this.location = response.data)
         //     // .then(response => console.log(this.location))
@@ -246,6 +318,10 @@ export default {
 
     .pac-item-query{
         font-size: 16px;
+    }
+
+    h3{
+        padding-top: 25px;
     }
 
 </style>
