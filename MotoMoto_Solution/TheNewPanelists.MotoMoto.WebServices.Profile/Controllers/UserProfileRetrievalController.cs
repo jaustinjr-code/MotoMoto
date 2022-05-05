dd43dc1e453eeefa97baf1a36343138f5639d033
Jacob Nagano-Sunia <jacob.sunia@student.csulb.edu>
2022-05-04 18:22:44 -0700

    created api for profile

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.WebServices.Profile/Controllers/UserProfileRetrievalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheNewPanelists.MotoMoto.WebServices.Profile.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
