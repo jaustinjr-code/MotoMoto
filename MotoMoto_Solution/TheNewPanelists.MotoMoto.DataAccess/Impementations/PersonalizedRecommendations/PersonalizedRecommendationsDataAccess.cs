ad24b48bb83b4ace76fcbbc76243198063443a06
kchu457 <60150323+kchu457@users.noreply.github.com>
2022-05-05 20:16:44 -0700

    missing semicolon

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.DataAccess/Impementations/PersonalizedRecommendations/PersonalizedRecommendationsDataAccess.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.DataAccess/Impementations/PersonalizedRecommendations/PersonalizedRecommendationsDataAccess.cs
@@ -6,7 +6,7 @@ namespace TheNewPanelists.MotoMoto.DataAccess.Impementations.PersonalizedRecomme
     public class PersonalizedRecommendationsDataAccess
     {
         private MySqlConnection? _mySqlConnection { get; set; }
-        private string _connectionString = "server=moto-moto.crd4iyvrocsl.us-west-1.rds.amazonaws.comp;user=dev_moto;database=pro_moto;port=3306;password=motomoto;"
+        private string _connectionString = "server=moto-moto.crd4iyvrocsl.us-west-1.rds.amazonaws.comp;user=dev_moto;database=pro_moto;port=3306;password=motomoto;";
         public bool EstablishMariaDBConnection()
         {
             try
