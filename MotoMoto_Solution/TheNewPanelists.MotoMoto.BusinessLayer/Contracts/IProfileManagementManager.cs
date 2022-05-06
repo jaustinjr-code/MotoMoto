78a2a8bb36e23c193b50baae9fa33810d5dfd0bf
Jacob Nagano-Sunia <jacob.sunia@student.csulb.edu>
2022-05-05 18:41:41 -0700

    uploaded IProfileManagerManager

---
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.BusinessLayer/Contracts/IProfileManagementManager.cs
@@ -0,0 +1,16 @@
+﻿using TheNewPanelists.MotoMoto.Models;
+
+namespace TheNewPanelists.MotoMoto.BusinessLayer
+{
+    public interface IProfileManagementManager
+    {
+        public ProfileModel CreatProfilesForAllNewAccountsManager();
+        public ProfileListModel RetrieveAllProfileManager();
+        public ProfileModel RetrieveSpecifiedProfileManager(string _username);
+        public ProfileModel RetrieveAllUpVotedPostsForProfileManager(string _username);
+        public ProfileModel UpdateProfileDescriptionManager(string _username, string _newDescription);
+        public ProfileModel UpdateProfileUsernameManager(string _username, string _newUsername);
+        public ProfileModel UpdateProfileStatusManager(string _username, bool _status);
+        public ProfileModel DeleteProfileManager(string _username);
+    }
+}
