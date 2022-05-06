b9fe8622d7517038b61482a0c1ae3544191bfc3c
kchu457 <60150323+kchu457@users.noreply.github.com>
2022-05-05 23:32:56 -0700

    Connection

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/noteDashboardConnection.js
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/noteDashboardConnection.js
@@ -5,7 +5,7 @@ axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'DELETE, POST, G
 axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With';
 const instance = axios.create({
     // baseURL: 'https://localhost:7261/api/'
-    baseURL: 'http://motomotoca.com:5225'
+    baseURL: 'http://motomotoca.com:5225/api/'
 });
 
 export {instance}
