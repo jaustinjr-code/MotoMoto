205f4a556461deb88ec74fbbdf31be490cfe2b6d
bobdidley <james.austin@student.csulb.edu>
2022-05-07 16:30:13 -0700

    UAD: Service Unit Tests

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/UsageAnalysisDashboardUnitTests/UsageAnalysisDashboardDataAccessUnitTests.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/UsageAnalysisDashboardUnitTests/UsageAnalysisDashboardDataAccessUnitTests.cs
@@ -9,9 +9,10 @@ namespace TheNewPanelists.MotoMoto.UnitTests.UsageAnalysisDashboardTests
     public class UsageAnalysisDashboardDataAccessUnitTests
     {
         private readonly IFetchKpiDataAccess _fetchBarViewKpiDataAccess;
+        private readonly IFetchKpiDataAccess _fetchBarFeedKpiDataAccess;
         private readonly IFetchKpiDataAccess _fetchTrendLoginKpiDataAccess;
         private readonly IFetchKpiDataAccess _fetchTrendRegistrationKpiDataAccess;
-        private readonly IFetchKpiDataAccess _fetchBarFeedKpiDataAccess;
+        private readonly IFetchKpiDataAccess _fetchTrendEventKpiDataAccess;
         // private readonly IFetchKpiDataAccess _submitKpiDataAccess;
         // private readonly IFetchKpiDataAccess _submitKpiDataAccess;
         public UsageAnalysisDashboardDataAccessUnitTests()
@@ -20,33 +21,35 @@ namespace TheNewPanelists.MotoMoto.UnitTests.UsageAnalysisDashboardTests
             _fetchBarFeedKpiDataAccess = new FetchBarKpiFeedDataAccess();
             _fetchTrendLoginKpiDataAccess = new FetchTrendKpiLoginDataAccess();
             _fetchTrendRegistrationKpiDataAccess = new FetchTrendKpiRegistrationDataAccess();
+            _fetchTrendEventKpiDataAccess = new FetchTrendKpiEventDataAccess();
             // _submitKpiDataAccess = new SubmitKpiDataAccess()
             // _submitKpiDataAccess = new SubmitKpiDataAccess()
         }
 
 
+        // Tests are too shallow and are prone to false positives
 
-        //[Fact]
-        //public void IsFetchChartDataValid_FetchBarViewKpiDataAccess_DisplayTotalNotNull()
-        //{
-        //// Arrange
-        //IUsageAnalyticModel model = new BarChartAnalyticModel("Views", "displayTotal");
-        //// Act
-        //IUsageAnalyticEntity result = _fetchBarViewKpiDataAccess.FetchChartMetrics(model);
-        //// Assert
-        //Assert.NotNull(result.metricList);
-        //}
+        [Fact]
+        public void IsFetchChartDataValid_FetchBarViewKpiDataAccess_DisplayTotalNotNull()
+        {
+            // Arrange
+            IUsageAnalyticModel model = new BarChartAnalyticModel("Views", "displayTotal");
+            // Act
+            IUsageAnalyticEntity result = _fetchBarViewKpiDataAccess.FetchChartMetrics(model);
+            // Assert
+            Assert.NotNull(result.metricList);
+        }
 
-        //[Fact]
-        //public void IsFetchChartDataValid_FetchBarFeedKpiDataAccess_FeedPostTotalNotNull()
-        //{
-        //// Arrange
-        //IUsageAnalyticModel model = new BarChartAnalyticModel("feedName", "feedPostTotal");
-        //// Act
-        //IUsageAnalyticEntity result = _fetchBarFeedKpiDataAccess.FetchChartMetrics(model);
-        //// Assert
-        //Assert.NotNull(result.metricList);
-        //}
+        [Fact]
+        public void IsFetchChartDataValid_FetchBarFeedKpiDataAccess_FeedPostTotalNotNull()
+        {
+            // Arrange
+            IUsageAnalyticModel model = new BarChartAnalyticModel("feedName", "feedPostTotal");
+            // Act
+            IUsageAnalyticEntity result = _fetchBarFeedKpiDataAccess.FetchChartMetrics(model);
+            // Assert
+            Assert.NotNull(result.metricList);
+        }
 
         [Fact]
         public void IsFetchChartDataValid_FetchTrendKpiLoginDataAccess_LoginTotalNotNull()
