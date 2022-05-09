b9a07461e6cc9ef9071e29c8e3fcd8ee62f185c3
DanBribi <80496947+Dbribs@users.noreply.github.com>
2022-05-09 16:17:38 -0700

    Updates for registration

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/components/RegistrationConfirmationComponent.vue
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/components/RegistrationConfirmationComponent.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="RegistrationView">
-        <h1>MotoMoto</h1>
-        {{this.message}}
-        <div v-if="success">
-        <button @click="this.$router.push('/login')">Go to Login</button>
+    <div id="confirmationview">
+    <h1 class="response">{{this.message}}</h1>
+        <div class="content">
+            <div class="success" v-if="success">
+                <h2 style="font-size: 20px;">You can change your username or password under the user profile section.</h2>
+       
+                <div style="margin-top: 40px;">
+                    <router-link to="/Login"><a class="login">Login</a></router-link>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -11,7 +16,7 @@
 <script>
 import { useCookies } from "vue3-cookies";
 import { defineComponent } from "vue";
-import { instance } from '../router/RegistrationConnection'
+import { instance } from '../router/RegistrationConnection';
 
 export default defineComponent({
     setup() {
@@ -32,8 +37,9 @@ export default defineComponent({
             {params: {email: this.email , registrationId: this.registrationId}}).then((response)=>{
                 console.log(`Server replied with: ${response.data}`);
                 this.message = response.data.message;
-                if(response.data.status == true)
+                if(response.data.status == true) {
                     this.success = true;
+                }
             }).catch((e)=>{
                 console.log(e);
                 this.message = e
@@ -41,9 +47,55 @@ export default defineComponent({
         },
     },
     created: function() {
+
         this.email = this.$route.query.email;
         this.registrationId = this.$route.query.registrationID;
-        this.Confirmation();
+
+        if((this.email === undefined) || (this.registrationId === undefined))
+        {
+            alert("Missing confirmation details. Redirecting to the home page.");
+            this.$router.push('/');
+        }
+        //else    
+            //this.Confirmation();
     }
 })
 </script>
+
+<style scoped>
+#confirmationview
+{
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    padding: 20px;
+}
+.content
+{
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    display: inline-block;
+    width: 500px;
+}
+h1.response 
+{
+    margin-top: 5px;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    font-size: 24px;
+}
+a.login 
+{
+    white-space: nowrap;
+    background-color:green;
+    width: fit-content;
+    border-width: 1px;
+    color: white;
+    text-align: center;
+    font-size: 16px;
+    cursor: pointer;
+    padding: 5px;
+}
+a.login:hover
+{
+    color: lightgreen;
+}
+</style>
