10d93cc6390ced9aed450f8d265ca42af112e0b6
bobdidley <james.austin@student.csulb.edu>
2022-05-09 03:24:08 -0700

    UAD: Submit KPI Business Layer Unit Tests created

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/UsageAnalysisDashboardUnitTests/UsageAnalysisDashboardBusinessLayerUnitTests.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.UnitTests/UsageAnalysisDashboardUnitTests/UsageAnalysisDashboardBusinessLayerUnitTests.cs
@@ -158,5 +158,101 @@ namespace TheNewPanelists.MotoMoto.UnitTests.UsageAnalysisDashboardTests
             // Assert
             Assert.False(response.isSuccess);
         }
+
+        [Fact]
+        public void IsAsyncCallReturnSuccessResponse_SubmitAdmissionKpiManagerLogin_ReturnTrue()
+        {
+            // Arrange
+            IUsageMetricModel model = new LoginUsageMetricModel(null, 10);
+            ISubmitKpiManager manager = new SubmitAdmissionKpiManager();
+            // Act
+            IResponseModel response = manager.IsSubmitKpiRequestValidAsync(model).Result;
+            // Assert
+            Assert.True(response.isSuccess);
+        }
+
+        [Fact]
+        public void IsAsyncCallReturnExceptionResponse_SubmitAdmissionKpiManagerLogin_ReturnFalse()
+        {
+            // Arrange
+            IUsageMetricModel model = new LoginUsageMetricModel(null, -1);
+            ISubmitKpiManager manager = new SubmitAdmissionKpiManager();
+            // Act
+            IResponseModel response = manager.IsSubmitKpiRequestValidAsync(model).Result;
+            // Assert
+            Assert.False(response.isComplete || response.isSuccess);
+        }
+
+        [Fact]
+        public void IsAsyncCallReturnSuccessResponse_SubmitAdmissionKpiManagerRegistration_ReturnTrue()
+        {
+            // Arrange
+            IUsageMetricModel model = new RegistrationUsageMetricModel(null, 10);
+            ISubmitKpiManager manager = new SubmitAdmissionKpiManager();
+            // Act
+            IResponseModel response = manager.IsSubmitKpiRequestValidAsync(model).Result;
+            // Assert
+            Assert.True(response.isSuccess);
+        }
+
+        [Fact]
+        public void IsAsyncCallReturnExceptionResponse_SubmitAdmissionKpiManagerRegistration_ReturnFalse()
+        {
+            // Arrange
+            IUsageMetricModel model = new RegistrationUsageMetricModel(null, -1);
+            ISubmitKpiManager manager = new SubmitAdmissionKpiManager();
+            // Act
+            IResponseModel response = manager.IsSubmitKpiRequestValidAsync(model).Result;
+            // Assert
+            Assert.False(response.isComplete || response.isSuccess);
+        }
+
+        [Fact]
+        public void IsAsyncCallReturnSuccessResponse_SubmitViewKpiManagerDisplay_ReturnTrue()
+        {
+            // Arrange
+            IUsageMetricModel model = new ViewUsageMetricModel("display", "Community Board", 1);
+            ISubmitKpiManager manager = new SubmitViewKpiManager();
+            // Act
+            IResponseModel result = manager.IsSubmitKpiRequestValidAsync(model).Result;
+            // Assert
+            Assert.True(result.isSuccess);
+        }
+
+        [Fact]
+        public void IsAsyncCallReturnExceptionResponse_SubmitViewKpiManagerDisplay_ReturnFalse()
+        {
+            // Arrange
+            IUsageMetricModel model = new ViewUsageMetricModel("displa", "Community Board", -1);
+            ISubmitKpiManager manager = new SubmitViewKpiManager();
+            // Act
+            IResponseModel result = manager.IsSubmitKpiRequestValidAsync(model).Result;
+            // Assert
+            Assert.False(result.isComplete || result.isSuccess);
+        }
+
+        [Fact]
+        public void IsAsyncCallReturnSuccessResponse_SubmitViewKpiManagerDuration_ReturnTrue()
+        {
+            // Arrange
+            IUsageMetricModel model = new ViewUsageMetricModel("duration", "Community Board", 1);
+            ISubmitKpiManager manager = new SubmitViewKpiManager();
+            // Act
+            IResponseModel result = manager.IsSubmitKpiRequestValidAsync(model).Result;
+            // Assert
+            Assert.True(result.isSuccess);
+        }
+
+        [Fact]
+        public void IsAsyncCallReturnExceptionResponse_SubmitViewKpiManagerDuration_ReturnFalse()
+        {
+            // Arrange
+            IUsageMetricModel model = new ViewUsageMetricModel("duratio", "Community Board", -1);
+            ISubmitKpiManager manager = new SubmitViewKpiManager();
+            // Act
+            IResponseModel result = manager.IsSubmitKpiRequestValidAsync(model).Result;
+            // Assert
+            Assert.False(result.isComplete || result.isSuccess);
+        }
     }
 }
