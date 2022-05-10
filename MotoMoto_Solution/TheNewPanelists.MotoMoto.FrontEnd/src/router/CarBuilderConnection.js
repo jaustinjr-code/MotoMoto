90f7f258815d07f068a0b4b180898b126ef6083d
Isabel Guzman <isa4509@gmail.com>
2022-05-10 15:44:00 -0700

    fixesbyblake may need to revert

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/CarBuilderConnection.js
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/CarBuilderConnection.js
@@ -4,12 +4,12 @@ axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'DELETE, POST, GET, OPTIONS';
 axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With';
 const instance = axios.create({
-    baseURL: 'https://localhost:44333/api/'
+    baseURL: 'http://localhost:44333/api/'
     //baseURL: 'http://motomotoca.com:5070/'
 });
 
 const instanceSubmit = axios.create({
-    baseURL: 'https://localhost:44333/api/'
+    baseURL: 'http://localhost:44333/api/'
     //baseURL: 'http://motomotoca.com:5070/'
 });
 
