6af01a5cb65641b7326700d30819eb481fa904e8
JustinCalma <calma.justin.00@gmail.com>
2022-05-10 15:24:43 -0700

    Finish creating a post and inserting into the datastore

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.WebServices.EventList/Controllers/EventListController.cs
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.WebServices.EventList/Controllers/EventListController.cs
@@ -16,7 +16,7 @@ namespace TheNewPanelists.MotoMoto.WebServices.EventList.Controllers
         private readonly EventPostContentDataAccess _eventPostContentDataAccess = new EventPostContentDataAccess();
 
         // Used to display the Index view of the project
-        //[HttpOptions]
+        [HttpOptions]
         public IActionResult Index()
         {
             return View(); // Display the view
@@ -24,7 +24,7 @@ namespace TheNewPanelists.MotoMoto.WebServices.EventList.Controllers
 
         // Web API call to fetch EventPostModel data from the data store and display it in the Frontend
         //[HttpGet]
-        [Route("GetEvents")]
+        [HttpGet, Route("GetEvents")]
         public IActionResult FetchAllEventPosts()
         {
             // Create dependency objects before performing operation
@@ -41,9 +41,29 @@ namespace TheNewPanelists.MotoMoto.WebServices.EventList.Controllers
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
                 return new StatusCodeResult(StatusCodes.Status500InternalServerError);
             }
+        }
+
+        // Gets the user input retrieved from the frontend and stores it within the datastore
+        [HttpPost, Route("CreateEvent")]
+        public IActionResult CreateEventPost(string time, string date, string streetAddress, string city, string state, string country, string zipCode, string title)
+        {
+            EventListService eventListService = new EventListService(_eventPostContentDataAccess);
+            EventListManager eventListManager = new EventListManager(eventListService);
+
+            try
+            {
+                // Make a call to the Event List Manager
+                EventDetailsModel postedEvent = eventListManager.CreateEventPost(time, date, streetAddress, city, state, country, zipCode, title);
+                // Return the fetched EventDetails Model
+                return Ok(postedEvent);
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
         }
 
     }
