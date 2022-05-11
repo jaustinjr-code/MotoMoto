374965f51b8e07c34789e231ab5907fb09e81366
Naeun Yu <56667626+ney827@users.noreply.github.com>
2022-05-11 13:18:07 -0700

    Connection

--- MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/NotificationSystemConnection.js
+++ MotoMoto_Solution/TheNewPanelists.MotoMoto.FrontEnd/src/router/NotificationSystemConnection.js
@@ -4,8 +4,8 @@ axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'DELETE, POST, GET, OPTIONS';
 axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With';
 const instance = axios.create({
-    baseURL: 'https://localhost:5000/'
-    // baseURL: 'https://motomotoca.com/'
+    //baseURL: 'https://localhost:5000/'
+    baseURL: 'https://motomotoca.com:5040/'
 });
 
 export {instance}
